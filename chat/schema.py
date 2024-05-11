6d0378372b053090747591bc752e859fc9c4a417
Pukhraj kumawat <pukhrajkumawat048@gmail.com>
2024-05-11 18:47:12 +0530

    added lots of changes

--- chat/schema.py
+++ chat/schema.py
@@ -3,7 +3,7 @@ from graphene_django import DjangoObjectType
 from graphene_django import DjangoListField
 from .models import Message,Group
 from account.models import customUser
-
+from django.db.models import Q
 
 
 class MessageType(DjangoObjectType):
@@ -22,15 +22,12 @@ class customUserType(DjangoObjectType):
         fields = ("id","username","mobile_no")
 
 
-
-
-
 class Query(graphene.ObjectType):
     all_messages = graphene.List(MessageType)
     one_sender = graphene.Field(customUserType,id=graphene.Int())
     one_recipient = graphene.Field(customUserType,id=graphene.Int())
-
-
+    chat_messages = graphene.List(MessageType,chat_user_id=graphene.Int(),your_user_id=graphene.Int())
+    
     def resolve_all_messages(root,info):
         return Message.objects.all()
     
@@ -39,7 +36,11 @@ class Query(graphene.ObjectType):
     
     def resolve_one_recipient(root,info,id):
         return customUser.objects.get(id=id)
-
+    
+    def resolve_chat_messages(root,info,chat_user_id,your_user_id):
+        chat_user = customUser.objects.get(id=chat_user_id)
+        your_user = customUser.objects.get(id=your_user_id)
+        return Message.objects.filter(Q(Q(sender=chat_user) & Q(recipient=your_user)) | Q(Q(sender=your_user) & Q(recipient=chat_user)) )
 
 
 schema = graphene.Schema(query=Query)
