14c1b6f0916e16d600168ff411d86e6a799225f7
Pukhraj kumawat <pukhrajkumawat048@gmail.com>
2024-05-15 08:48:01 +0530

    upadte virtual env

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import AuthPage from './components/auth'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <>
-    <h1 className='bg-green-500'>hello my project</h1>
+  <AuthPage/>
   </>
   )
 }
